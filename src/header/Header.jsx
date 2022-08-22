6660dbb19aa2514b367b8cb544f903ead6b59566
Aryash Jain <aryash.enu@gmail.com>
2022-08-22 09:50:14 +0530

    first commit

---
+++ src/header/Header.jsx
@@ -0,0 +1,18 @@
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <div className="headerTitles">
+        <span className='headerTitleLg'>Entrepreneurship-Cell</span>
+        <span className='headerTitleMd'><br/>IET Lucknow</span>
+        <span className='headerTitleSm'>Where Ideas breathe<br></br></span>
+     
+     
+      </div>
+      
+
+
+    </div>
+  )
+}
---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import './sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+           <div className="sidebarTitle">About Us</div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzQmuasfK1qjFkjJL-4h21Yps4e3i0fpHBBQ&usqp=CAU" alt="" />
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+             eius nesciunt quas, reprehenderit tempore sequi omnis reiciendis 
+             \fugiat deserunt autem nostrum dolorem cupiditate repudiandae eligendi consequatur 
+             optio iusto nam vitae.</p>
+        </div>
+         
+    );
+}
+
+export default Sidebar;
